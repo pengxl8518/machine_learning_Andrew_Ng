acd02f0977a53b47f82661480882076b3da62ba0
pengxingliang <pengxl@sunmnet.com>
2018-11-30 17:44:57 +0800

    Initial commit

---
+++ ex3_neural network/neural network.py
@@ -0,0 +1,202 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import matplotlib
+import scipy.optimize as opt
+from sklearn.metrics import classification_report#这个包是评价报告
+
+
+def load_data(path, transpose=True):
+    data = sio.loadmat(path)
+    y = data.get('y')  # (5000,1)
+    y = y.reshape(y.shape[0])  # make it back to column vector
+
+    X = data.get('X')  # (5000,400)
+
+    if transpose:
+        # for this dataset, you need a transpose to get the orientation right
+        X = np.array([im.reshape((20, 20)).T for im in X])
+
+        # and I flat the image again to preserve the vector presentation
+        X = np.array([im.reshape(400) for im in X])
+
+    return X, y
+# 数据集说明，X为像素的2值化值，y为数字真实值（1-10），每个数字出现500次
+X, y = load_data('ex3data1.mat')
+
+print(X.shape)
+print(y.shape)
+
+def plot_an_image(image):
+#     """
+#     image : (400,)
+#     """
+    fig, ax = plt.subplots(figsize=(1, 1))
+    ax.matshow(image.reshape((20, 20)), cmap=matplotlib.cm.binary)
+    plt.xticks(np.array([]))  # just get rid of ticks
+    plt.yticks(np.array([]))
+
+def plot_100_image(X):
+    """ sample 100 image and show them
+    assume the image is square
+
+    X : (5000, 400)
+    """
+    size = int(np.sqrt(X.shape[1]))
+
+    # sample 100 image, reshape, reorg it
+    sample_idx = np.random.choice(np.arange(X.shape[0]), 100)  # 100*400
+    sample_images = X[sample_idx, :]
+
+    fig, ax_array = plt.subplots(nrows=10, ncols=10, sharey=True, sharex=True, figsize=(8, 8))
+
+    for r in range(10):
+        for c in range(10):
+            ax_array[r, c].matshow(sample_images[10 * r + c].reshape((size, size)),
+                                   cmap=matplotlib.cm.binary)
+            plt.xticks(np.array([]))
+            plt.yticks(np.array([]))
+            #绘图函数，画100张图片
+
+raw_X, raw_y = load_data('ex3data1.mat')
+print(raw_X.shape)
+print(raw_y.shape)
+
+
+# add intercept=1 for x0
+#为x0进行初始化，这里全部设置为0
+X = np.insert(raw_X, 0, values=np.ones(raw_X.shape[0]), axis=1)#插入了第一列（全部为1）
+X.shape
+
+
+# y have 10 categories here. 1..10, they represent digit 0 as category 10 because matlab index start at 1
+# I'll ditit 0, index 0 again
+y_matrix = []
+
+#将y值从1-10的真实值，映射成一个10×5000维的矩阵，每一行代表一个值的0/1映射转换
+for k in range(1, 11):
+    y_matrix.append((raw_y == k).astype(int))
+
+# last one is k==10, it's digit 0, bring it to the first position，最后一列k=10，都是0，把最后一列放到第一列
+#把数字10的位置换到最后
+y_matrix = [y_matrix[-1]] + y_matrix[:-1]
+y = np.array(y_matrix)
+
+y.shape
+
+# 扩展 5000*1 到 5000*10
+#     比如 y=10 -> [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]: ndarray
+#
+
+
+
+# 训练一维模型 train 1 model
+#即每次只对一个类别进行分类，并评估其预测准确度
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+# 损失函数
+def cost(theta, X, y):
+    ''' cost fn is -l(theta) for you to minimize'''
+    return np.mean(-y * np.log(sigmoid(X @ theta)) - (1 - y) * np.log(1 - sigmoid(X @ theta)))
+
+# 正则化损失函数
+
+def regularized_cost(theta, X, y, l=1):
+    '''you don't penalize theta_0'''
+    theta_j1_to_n = theta[1:]
+    regularized_term = (l / (2 * len(X))) * np.power(theta_j1_to_n, 2).sum()
+    return cost(theta, X, y) + regularized_term
+
+def gradient(theta, X, y):
+    '''just 1 batch gradient'''
+    return (1 / len(X)) * X.T @ (sigmoid(X @ theta) - y)
+
+def regularized_gradient(theta, X, y, l=1):
+    '''still, leave theta_0 alone'''
+    theta_j1_to_n = theta[1:]
+    regularized_theta = (l / len(X)) * theta_j1_to_n
+
+    # by doing this, no offset is on theta_0
+    # concatenate  合并函数->添加上缺失掉的theat[0]函数
+    regularized_term = np.concatenate([np.array([0]), regularized_theta])
+    return gradient(theta, X, y) + regularized_term
+
+def logistic_regression(X, y, l=1):
+    """generalized logistic regression
+    args:
+        X: feature matrix, (m, n+1) # with incercept x0=1
+        y: target vector, (m, )
+        l: lambda constant for regularization
+
+    return: trained parameters
+    """
+    # init theta
+    theta = np.zeros(X.shape[1])
+
+    # train it
+    res = opt.minimize(fun=regularized_cost,
+                       x0=theta,
+                       args=(X, y, l),
+                       method='TNC',
+                       jac=regularized_gradient,
+                       options={'disp': True})
+    # get trained parameters
+    final_theta = res.x
+
+    return final_theta
+
+def predict(x, theta):
+    prob = sigmoid(x @ theta)
+    return (prob >= 0.5).astype(int)
+# 调用带正则化的逻辑递归函数，得出训练出theta值，改值会使得模型的损失函数最小。
+# 输入值为(X,y[0])、也即只对数字0值进行预测
+t0 = logistic_regression(X, y[0])
+print(t0.shape)
+# 预测对数字0的预测准确率
+y_pred = predict(X, t0)
+print('Accuracy={}'.format(np.mean(y[0] == y_pred)))
+
+
+# 训练k维模型 train k model
+# 即对每一个数字进行预测
+# 返回经训练后十维数据的theta值
+k_theta = np.array([logistic_regression(X, y[k]) for k in range(10)])
+print(k_theta.shape)
+prob_matrix = sigmoid(X @ k_theta.T)
+np.set_printoptions(suppress=True)
+# 这里用的很巧妙，取沿轴最大值的索引
+y_pred = np.argmax(prob_matrix, axis=1)#返回沿轴axis最大值的索引，axis=1代表行
+# 去y的真实值并将其的10代换为0
+y_answer = raw_y.copy()
+y_answer[y_answer==10] = 0
+print(classification_report(y_answer, y_pred))
+
+
+###################
+# 神经网络
+
+
+def load_weight(path):
+    data = sio.loadmat(path)
+    return data['Theta1'], data['Theta2']
+
+theta1, theta2 = load_weight('ex3weights.mat')
+theta1.shape, theta2.shape
+X, y = load_data('ex3data1.mat',transpose=False)
+X = np.insert(X, 0, values=np.ones(X.shape[0]), axis=1)  # intercept
+X.shape, y.shape
+a1 = X
+z2 = a1 @ theta1.T # (5000, 401) @ (25,401).T = (5000, 25)
+z2 = np.insert(z2, 0, values=np.ones(z2.shape[0]), axis=1)
+a2 = sigmoid(z2)
+a2.shape
+z3 = a2 @ theta2.T
+z3.shape
+a3 = sigmoid(z3)
+a3
+y_pred = np.argmax(a3, axis=1) + 1  # numpy is 0 base index, +1 for matlab convention，返回沿轴axis最大值的索引，axis=1代表行
+y_pred.shape
+print(classification_report(y, y_pred))
+
---
+++ ex5-bias vs variance/ML-Exercise5.py
@@ -0,0 +1,240 @@
+import numpy as np
+import scipy.io as sio
+import scipy.optimize as opt
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def load_data():
+    """for ex5
+    d['X'] shape = (12, 1)
+    pandas has trouble taking this 2d ndarray to construct a dataframe, so I ravel
+    the results
+    """
+    d = sio.loadmat('ex5data1.mat')
+    return map(np.ravel, [d['X'], d['y'], d['Xval'], d['yval'], d['Xtest'], d['ytest']])
+
+X, y, Xval, yval, Xtest, ytest = load_data()
+
+df = pd.DataFrame({'water_level':X, 'flow':y})
+# (x轴，y轴，数据)
+sns.lmplot('water_level', 'flow', data=df, fit_reg=False, size=7)
+plt.show()
+
+# insert(待插入的数据，插入的位置，准备插入的数据，沿什么轴插入)
+X, Xval, Xtest = [np.insert(x.reshape(x.shape[0], 1), 0, np.ones(x.shape[0]), axis=1) for x in (X, Xval, Xtest)]
+
+def cost(theta, X, y):
+    """
+    X: R(m*n), m records, n features
+    y: R(m)
+    theta : R(n), linear regression parameters
+    """
+    m = X.shape[0]
+
+    inner = X @ theta - y  # R(m*1)
+
+    # 1*m @ m*1 = 1*1 in matrix multiplication
+    # but you know numpy didn't do transpose in 1d array, so here is just a
+    # vector inner product to itselves
+    square_sum = inner.T @ inner
+    cost = square_sum / (2 * m)
+
+    return cost
+
+theta = np.ones(X.shape[1])
+cost(theta, X, y)
+
+def gradient(theta, X, y):
+    m = X.shape[0]
+
+    inner = X.T @ (X @ theta - y)  # (m,n).T @ (m, 1) -> (n, 1)
+
+    return inner / m
+
+gradient(theta, X, y)
+
+
+def regularized_gradient(theta, X, y, l=1):
+    m = X.shape[0]
+
+    regularized_term = theta.copy()  # same shape as theta
+    regularized_term[0] = 0  # don't regularize intercept theta
+
+    regularized_term = (l / m) * regularized_term
+
+    return gradient(theta, X, y) + regularized_term
+
+regularized_gradient(theta, X, y)
+
+
+# 该函数用于求解训练后的theta值
+def linear_regression_np(X, y, l=1):
+    """linear regression
+    args:
+        X: feature matrix, (m, n+1) # with incercept x0=1
+        y: target vector, (m, )
+        l: lambda constant for regularization
+
+    return: trained parameters
+    """
+    # init theta
+    theta = np.ones(X.shape[1])
+
+    # train it
+    res = opt.minimize(fun=regularized_cost,
+                       x0=theta,
+                       args=(X, y, l),
+                       method='TNC',
+                       jac=regularized_gradient,
+                       options={'disp': True})
+    return res
+
+def regularized_cost(theta, X, y, l=1):
+    m = X.shape[0]
+
+    regularized_term = (l / (2 * m)) * np.power(theta[1:], 2).sum()
+
+    return cost(theta, X, y) + regularized_term
+
+theta = np.ones(X.shape[0])
+
+final_theta = linear_regression_np(X, y, l=0).get('x')
+
+b = final_theta[0] # intercept
+m = final_theta[1] # slope
+
+# 可视化训练数据
+plt.scatter(X[:,1], y, label="Training data")
+# 可视化代入求解出theta后假设函数
+plt.plot(X[:, 1], X[:, 1]*m + b, label="Prediction")
+# label的位置 loc = 2在左上角
+plt.legend(loc=2)
+plt.show()
+
+training_cost, cv_cost = [], []
+
+m = X.shape[0]
+
+# 得出 训练集和交叉测试集的regularized_cost(res.x, X[:i, :], y[:i], l=0) for i in X.shape=[0]值
+
+for i in range(1, m + 1):
+    #     print('i={}'.format(i))
+    res = linear_regression_np(X[:i, :], y[:i], l=0)
+
+    tc = regularized_cost(res.x, X[:i, :], y[:i], l=0)
+    cv = regularized_cost(res.x, Xval, yval, l=0)
+    #     print('tc={}, cv={}'.format(tc, cv))
+    training_cost.append(tc)
+    cv_cost.append(cv)
+# 可视化测试集的损失值(x轴为 i in X.shape[0])
+plt.plot(np.arange(1, m+1), training_cost, label='training cost')
+# 得出交叉测试集的损失值(x轴为 i in X.shape[0])
+plt.plot(np.arange(1, m+1), cv_cost, label='cv cost')
+plt.legend(loc=1)
+plt.show()
+
+def prepare_poly_data(*args, power):
+    """
+    args: keep feeding in X, Xval, or Xtest
+        will return in the same order
+    """
+    def prepare(x):
+        # expand feature
+        df = poly_features(x, power=power)
+
+        # normalization
+        ndarr = normalize_feature(df).as_matrix()
+
+        # add intercept term
+        return np.insert(ndarr, 0, np.ones(ndarr.shape[0]), axis=1)
+
+    return [prepare(x) for x in args]
+
+#创建多项式特征
+# 给其进行元素乘方
+def poly_features(x, power, as_ndarray=False):
+    data = {'f{}'.format(i): np.power(x, i) for i in range(1, power + 1)}
+    df = pd.DataFrame(data)
+
+    return df.as_matrix() if as_ndarray else df
+
+X, y, Xval, yval, Xtest, ytest = load_data()
+
+poly_features(X, power=3)
+
+# 准备多项式回归数据
+
+#对数据进行按列标准化 按列的形式
+def normalize_feature(df):
+    """Applies function along input axis(default 0) of DataFrame."""
+    return df.apply(lambda column: (column - column.mean()) / column.std())
+
+X_poly, Xval_poly, Xtest_poly= prepare_poly_data(X, Xval, Xtest, power=8)
+X_poly[:3, :]
+
+# 画出学习曲线
+def plot_learning_curve(X, y, Xval, yval, l=0):
+    training_cost, cv_cost = [], []
+    m = X.shape[0]
+
+    for i in range(1, m + 1):
+        # regularization applies here for fitting parameters
+        res = linear_regression_np(X[:i, :], y[:i], l=l)
+
+        # remember, when you compute the cost here, you are computing
+        # non-regularized cost. Regularization is used to fit parameters only
+        tc = cost(res.x, X[:i, :], y[:i])
+        cv = cost(res.x, Xval, yval)
+
+        training_cost.append(tc)
+        cv_cost.append(cv)
+
+    plt.plot(np.arange(1, m + 1), training_cost, label='training cost')
+    plt.plot(np.arange(1, m + 1), cv_cost, label='cv cost')
+    plt.legend(loc=1)
+# 得到l = 0 时 训练集 和交叉测试在多项回归后的损失值
+plot_learning_curve(X_poly, y, Xval_poly, yval, l=0)
+plt.show()
+
+# 得到l = 1时 训练集 和交叉测试在多项回归后的损失值
+plot_learning_curve(X_poly, y, Xval_poly, yval, l=1)
+plt.show()
+
+# 得到l = 100 时 训练集 和交叉测试在多项回归后的损失值
+plot_learning_curve(X_poly, y, Xval_poly, yval, l=100)
+plt.show()
+
+#取一系列的l 值，找出其中使得交叉测试集损失值最小的
+
+l_candidate = [0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10]
+training_cost, cv_cost = [], []
+
+for l in l_candidate:
+    res = linear_regression_np(X_poly, y, l)
+
+    tc = cost(res.x, X_poly, y)
+    cv = cost(res.x, Xval_poly, yval)
+
+    training_cost.append(tc)
+    cv_cost.append(cv)
+
+plt.plot(l_candidate, training_cost, label='training')
+plt.plot(l_candidate, cv_cost, label='cross validation')
+plt.legend(loc=2)
+
+plt.xlabel('lambda')
+
+plt.ylabel('cost')
+plt.show()
+
+# best cv I got from all those candidates
+l_candidate[np.argmin(cv_cost)]
+
+# use test data to compute the cost
+# 找出上述l值中使得测试值最小的
+for l in l_candidate:
+    theta = linear_regression_np(X_poly, y, l).x
+    print('test cost(l={}) = {}'.format(l, cost(theta, Xtest_poly, ytest)))
+
+# 调参后， $\lambda = 0.3$ 是最优选择，这个时候测试代价最小
